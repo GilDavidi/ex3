8e3e69ddd9ab4d330bd51c73f0c86eb3bcef18f8
GilDavidi <95719086+GilDavidi@users.noreply.github.com>
2022-04-14 15:56:44 +0300

    Add files via upload

--- ex3/js/scripts.js
+++ ex3/js/scripts.js
@@ -1,60 +1,64 @@
 
-  window.onload =() => {
-      let secsions="";
-      let boxes=document.getElementsByClassName("layout3_main_boxes");
-      let opacity=0;
-      for(i=0; i<200;i++) 
-      {
-        secsions+= "<section class=" +"layout3_main_boxes"+"></section>";
-      }
+window.onload = () => {
+  let main = document.getElementsByTagName("main")[0];
+  let boxes = document.getElementsByClassName("boxes")[0];
+  let firstBox = document.getElementsByClassName("layout_3_box4")[0];
+  let opacity = 0;
+  for (i = 0; i < 50; i++) {
+    let clone = boxes.cloneNode(true); // "deep" clone
+    main.appendChild(clone);
+  }
 
-      document.getElementsByTagName("main")[0].innerHTML=secsions;
-      for(i=0,j=0; i<200, j<1;i++, j+=0.005)
-      {
-        boxes[i].style.opacity=j;
-      }
+  document.getElementById("plus").onclick = () => {
 
-
- 
-      document.getElementById("plus").onclick = () =>
-      {
-        opacity+=0.025;
-        if(opacity<0.95)
-        {
-          boxes[0].style.opacity=opacity;
-        }
-        else
-        {
-          boxes[0].style.opacity=0;
-          opacity=0;
-        }
-      }
-
-
-    document.body.onclick = function(e) 
-    {   //when the document body is clicked
-      if (window.event) {
-          e = event.srcElement;           //assign the element clicked to e (IE 6-8)
-      }
-      else
-      {
-          e = e.target;                   //assign the element clicked to e
-      }
-  
-      if (e.className && e.className.indexOf('layout3_main_boxes') != -1)
-      {
-      const backgroundColor = e.style.backgroundColor; 
-      let sectionColor = window.getComputedStyle(e).backgroundColor;
-      if (sectionColor.indexOf("rgb(0, 0, 0)") != -1)
-      {
-         e.style.backgroundColor = "#ffcdd2";
-      } 
-      else 
-      {
-        e.style.backgroundColor = "black";
-      }
+    if (firstBox.style.opacity < 0.95) {
+      opacity += 0.25;
+      firstBox.style.opacity = opacity;
     }
-        
-     }
+    else {
+      firstBox.style.opacity = 0;
+      opacity = 0;
+    }
+  }
+
+  document.body.onclick = function (e) {   //when the document body is clicked
+    if (window.event) {
+      e = event.srcElement;         //assign the element clicked to e (IE 6-8)
+    }
+
+
+    if (e.className) {
+      if (e.className.indexOf("layout_3_box1") != -1 || e.className.indexOf("layout_3_box2") != -1
+        || e.className.indexOf("layout_3_box3") != -1 || e.className.indexOf("layout_3_box4") != -1) {
+        let PinkToBlack = false;
+        let BlackToPink = false;
+        let sectionColor = window.getComputedStyle(e).backgroundColor;
+        if (sectionColor.indexOf("rgb(0, 0, 0)") != -1) {
+          e.style.backgroundColor = "pink";
+          BlackToPink = true;
+        }
+        else {
+          e.style.backgroundColor = "black";
+          PinkToBlack = true;
+        }
+        e.addEventListener('mouseover', function handleMouseOver() {
+          e.style.backgroundColor = "pink";
+        });
+        e.addEventListener('mouseout', function handleMouseOut() {
+          if (BlackToPink) {
+            e.style.backgroundColor = "pink";
+          }
+          if (PinkToBlack) {
+            e.style.backgroundColor = "black";
+          }
+          if (!PinkToBlack && !BlackToPink) {
+            e.style.backgroundColor = "black";
+          }
+
+        });
+      }
+
+    }
+  }
 
 }
